43f45b36dd8f66232bbda4fd76d02586510c7aae
paulo arthur beck <pauloabeck@gmail.com_>
2021-07-19 23:44:34 +0200

    refactored to use exchangeToMono(..) instead of exchange() (depracated).

--- complete/src/main/java/hello/GreetingWebClient.java
+++ complete/src/main/java/hello/GreetingWebClient.java
@@ -1,20 +1,32 @@
 package hello;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import reactor.core.publisher.Mono;
 
 public class GreetingWebClient {
-	private WebClient client = WebClient.create("http://localhost:8080");
 
-	private Mono<ClientResponse> result = client.get()
-			.uri("/hello")
-			.accept(MediaType.TEXT_PLAIN)
-			.exchange();
+	private final String result;
+
+	public GreetingWebClient() {
+
+		WebClient client = WebClient.create("http://localhost:8080");
+		WebClient.RequestHeadersUriSpec<?> requestHeadersUriSpec = client.get();
+		requestHeadersUriSpec.uri("/hello").accept(MediaType.TEXT_PLAIN);
+		Mono<String> response = requestHeadersUriSpec.exchangeToMono(res -> {
+			if (res.statusCode().equals(HttpStatus.OK))
+				return res.bodyToMono(String.class);
+			if (res.statusCode().is4xxClientError())
+				return Mono.just("Error response: ".concat(res.statusCode().getReasonPhrase()));
+			return res.createException().flatMap(Mono::error);
+		});
+
+		result = response.block();
+
+	}
 
 	public String getResult() {
-		return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
+		return result;
 	}
 }
