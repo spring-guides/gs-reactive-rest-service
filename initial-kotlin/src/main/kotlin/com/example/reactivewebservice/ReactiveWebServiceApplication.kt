dc37802ee39b8836aabca7975380245c9ea0c79d
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-03-02 16:29:03 -0700

    Align initial-kotlin with Initializr
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

---
+++ initial-kotlin/src/main/kotlin/com/example/reactivewebservice/ReactiveWebServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.reactivewebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
