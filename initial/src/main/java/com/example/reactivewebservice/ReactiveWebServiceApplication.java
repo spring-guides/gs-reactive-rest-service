ae818fb3ed6dac4ae7a1bb9758ef79f47d73f61c
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-03-02 16:23:52 -0700

    Align initial with Initializr
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

---
+++ initial/src/main/java/com/example/reactivewebservice/ReactiveWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.reactivewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebServiceApplication.class, args);
+	}
+}
